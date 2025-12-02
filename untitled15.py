8cea6e642c7307aa1d16a47125dc7dec8cdf71ee
Shraddha17705 <hanagarsunita46@gmail.com>
2025-12-02 14:12:45 +0530

    Add files via upload

---
+++ untitled15.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import random
+from sklearn.decomposition import KNN
+import np.random
+num=np.random(100)
+def kNN():
+    data=np.random(100).num
+    label=data.target
+    k_nearest_neighbour_label_name=data.target_name
+    x_train,x_test,y_train,y_test ==c[data,column=random(42)]
+    for i,numn in range(1-50):
+        if x[i] <= 0.5:
+            x[i] = x_test
+        else:
+            x[i]=y_test
+    return x[i]
+clf = np.num.kNN()
+clf_df=np.DataFrame(data_rd,columns =['Class 1','Class 2'])
+for all values in x_test:
+    x_test = 'Class 1'
+return x_test
+for all values in y_test:
+    y_test = 'Class 2'
+return y_test
+print(f"Class 1:",x_test)
+print(f"Class 2:",y_test)
+
+for i,values=(50-100):
+    value[i]=clf[i]
+return value
+
+for values=(1,2,3,4,5,20,30):
+    num=np.clf[i]
+    print("KNN performed for %d : ",i)
+    print(num)
+return 0
+
+plt.figure(figsize =(12,8))
+plt.scatter(x_test,y_test,color='red',alpha=0.5,fmt='.2f',linewidth=0.5)
+plt.title("K-Nearest Neighbour Algorithm")
+plt.legend()
+plt.show()
+
+plt.scatter(clf,color="blue",annot=True,linewidth=0.5,fmt='.2f')
+plt.legend()
+plt.show()
